2cd6846886d6a6fe6382aa28cdaafb5d12dd3ec6
ZainAnjum <zainanjum100@gmail.com>
2018-02-25 20:46:06 +0500

    Initial Commit

---
+++ fbMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fbMessenger
+//
+//  Created by ZainAnjum on 25/02/2018.
+//  Copyright © 2018 ZainAnjum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
